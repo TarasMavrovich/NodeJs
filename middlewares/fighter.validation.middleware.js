4579c8734c8917b13fa71a7709a859fe73c06f50
TarasMavrovich <taras.mavrovich@gmail.com>
2023-01-24 04:03:07 +0200

    added validation for fighter

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -15,6 +15,25 @@ const createFighterValid = (req, res, next) => {
 
   if (!req.body.name || !req.body.power || !req.body.defense) {
     res.status(400).send( { message: 'All fields must be filled' })
+} else {
+  if (JSON.stringify(reqFighter) == JSON.stringify(req.body) || JSON.stringify(reqFighter1) == JSON.stringify(req.body)) {
+    if (req.body.defense <= 100 && req.body.defense >= 1) {
+      if (req.body.power <= 100 && req.body.power >= 1) {
+        if ((req.body.health <= 120 && req.body.health >= 80) || !req.body.health) {
+          next()
+        } else {
+            res.status(400).send("health - 80 < health < 120")
+          }
+      } else {
+          res.status(400).send("power - 1 < power < 100")
+        }
+    } else {
+        res.status(400).send("defense - 1 < defense < 100")
+      }
+  } else {
+      res.status(400).send("added extra fields")
+  }
+
 }
   // TODO: Implement validatior for FIGHTER entity during creation
   next();
@@ -27,7 +46,6 @@ const updateFighterValid = (req, res, next) => {
     res.status(400).send( { message: 'When updating fighter, there must be at least one field' } )
   }
   // TODO: Implement validatior for FIGHTER entity during update
-  next();
 };
 
 export { createFighterValid, updateFighterValid };
